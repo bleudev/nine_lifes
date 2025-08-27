950696f6356ebe6a8b4347ce29c843f3037c1515
bleudev <aitiiigg1@gmail.com>
2025-08-27 12:45:28 +0300

    done center heart

--- src/main/java/com/bleudev/nine_lifes/mixin/InGameHudMixin.java
+++ src/main/java/com/bleudev/nine_lifes/mixin/InGameHudMixin.java
@@ -1,24 +1,44 @@
 package com.bleudev.nine_lifes.mixin;
 
 import com.bleudev.nine_lifes.util.LivesUtils;
+import com.mojang.blaze3d.pipeline.RenderPipeline;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gl.RenderPipelines;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.RenderLayers;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.ColorHelper;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
+    @Unique
+    private void drawCenterHeart(DrawContext context, Identifier texture, int lives) {
+        int x0 = context.getScaledWindowWidth() / 2;
+        int y = context.getScaledWindowHeight() - 48;
+
+        float delta = (float) lives / 9;
+
+        context.drawTexture(
+            RenderPipelines.GUI_TEXTURED, texture,
+            x0 - 9, y - 5, 0, 0, 18, 18, 18, 18,
+            ColorHelper.fromFloats(0.75f, delta, delta, delta)
+        );
+    }
+
     @Inject(method = "renderStatusBars", at = @At("RETURN"), cancellable = true)
     private void renderCounter(DrawContext context, CallbackInfo ci) {
         MinecraftClient client = MinecraftClient.getInstance();
         if (client.player == null) ci.cancel();
-
-        System.out.println("render!");
 
         TextRenderer textRenderer = client.textRenderer;
 
@@ -28,18 +48,19 @@ public class InGameHudMixin {
 
         Text text = Text.literal("" + lives);
 
-        // координаты по умолчанию — по центру, над хотбаром
-        int x = context.getScaledWindowWidth() / 2 - textRenderer.getWidth(text) / 2;
+        int x0 = context.getScaledWindowWidth() / 2;
+        int x = x0 - textRenderer.getWidth(text) / 2;
         int y = context.getScaledWindowHeight() - 48;
 
-        // если есть уровень опыта — чуть выше, чтобы не наезжало
-        if (client.player.experienceLevel > 0) {
-            y -= 6;
-        }
+//        if (client.player.experienceLevel > 0) {
+//            y -= 6;
+//        }
 
         context.getMatrices().pushMatrix();
-        context.getMatrices().translate(0, 0); // поднять по Z, чтобы было поверх
-        context.drawTextWithShadow(textRenderer, text, x, y, 0xFFFFFFff);
+        context.getMatrices().translate(0, 0);
+        drawCenterHeart(context, Identifier.ofVanilla("textures/gui/sprites/hud/heart/container_hardcore.png"), lives);
+        drawCenterHeart(context, Identifier.ofVanilla("textures/gui/sprites/hud/heart/hardcore_full.png"), lives);
+        context.drawTextWithShadow(textRenderer, text, x, y, 0xffffffff);
         context.getMatrices().popMatrix();
     }
 
