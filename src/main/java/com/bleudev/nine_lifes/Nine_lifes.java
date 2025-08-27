b8dbbca71d2f0f5289278b96a91eabc83de66ff1
bleudev <aitiiigg1@gmail.com>
2025-08-27 19:02:46 +0300

    commands updates

--- src/main/java/com/bleudev/nine_lifes/Nine_lifes.java
+++ src/main/java/com/bleudev/nine_lifes/Nine_lifes.java
@@ -1,7 +1,9 @@
 package com.bleudev.nine_lifes;
 
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.server.command.CommandManager;
 
 public class Nine_lifes implements ModInitializer {
@@ -13,7 +15,21 @@ public class Nine_lifes implements ModInitializer {
                 .literal("nl").executes(CustomCommands::nine_lifes)
                 .then(CommandManager
                     .literal("reset").executes(CustomCommands::nine_lifes_reset)
-                ));
+                    .then(CommandManager
+                        .argument("players", EntityArgumentType.players())
+                        .executes(CustomCommands::nine_lifes_reset_player))
+                )
+                .then(CommandManager
+                    .literal("set").executes(CustomCommands::nine_lifes_set)
+                    .then(CommandManager
+                        .argument("lives", IntegerArgumentType.integer(1, 9))
+                        .executes(CustomCommands::nine_lifes_set_lives)
+                        .then(CommandManager
+                            .argument("players", EntityArgumentType.players())
+                            .executes(CustomCommands::nine_lifes_set_lives_players))
+                    )
+                )
+            );
         });
     }
 }
