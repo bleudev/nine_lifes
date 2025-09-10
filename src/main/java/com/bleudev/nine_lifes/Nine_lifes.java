a245e9c2f688b5549c39c5adb1913554313db0fd
bleudev <aitiiigg1@gmail.com>
2025-09-10 20:22:10 +0300

    Dump version to 1.4.1

--- src/main/java/com/bleudev/nine_lifes/Nine_lifes.java
+++ src/main/java/com/bleudev/nine_lifes/Nine_lifes.java
@@ -25,7 +25,7 @@ public class Nine_lifes implements ModInitializer {
     public static final String MOD_ID = "nine_lifes";
     public static final String NAME = "Nine lifes";
     public static final String AUTHOR = "bleudev";
-    public static final String VERSION = "1.4";
+    public static final String VERSION = "1.4.1";
     public static final String GITHUB_LINK = "https://github.com/bleudev/nine_lifes";
     public static final String MODRINTH_LINK = "https://modrinth.com/mod/nine_lifes";
 
