852c1414a340895c0a221f609889d6e9ef110635
bleudev <aitiiigg1@gmail.com>
2025-10-09 20:02:10 +0300

    second hotfix

--- src/main/java/com/bleudev/nine_lifes/Nine_lifes.java
+++ src/main/java/com/bleudev/nine_lifes/Nine_lifes.java
@@ -28,7 +28,7 @@ public class Nine_lifes implements ModInitializer {
     public static final String MOD_ID = "nine_lifes";
     public static final String NAME = "Nine lifes";
     public static final String AUTHOR = "bleudev";
-    public static final String VERSION = "1.7.1";
+    public static final String VERSION = "1.7.1b";
     public static final String GITHUB_LINK = "https://github.com/bleudev/nine_lifes";
     public static final String MODRINTH_LINK = "https://modrinth.com/mod/nine_lifes";
 
