158c74603b8f7901dc90515eb09114c80112e378
bleudev <aitiiigg1@gmail.com>
2025-10-09 12:47:28 +0300

    fixes #12

---
+++ src/main/java/com/bleudev/nine_lifes/client/compat/modmenu/NineLifesConfig.java
@@ -0,0 +1,8 @@
+package com.bleudev.nine_lifes.client.compat.modmenu;
+
+import eu.midnightdust.lib.config.MidnightConfig;
+
+public class NineLifesConfig extends MidnightConfig {
+    @Entry
+    public static boolean join_message_enabled = true;
+}
