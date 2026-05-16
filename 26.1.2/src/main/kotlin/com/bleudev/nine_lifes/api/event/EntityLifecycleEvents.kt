07af0985c6fd977e1d90ae71fec3e2f9340e2286
bleudev <aitiiigg1@gmail.com>
2026-05-16 14:11:58 +0300

    Use new Fabric allow load event

--- 26.1.2/src/main/kotlin/com/bleudev/nine_lifes/api/event/EntityLifecycleEvents.kt
+++ 26.1.2/src/main/kotlin/com/bleudev/nine_lifes/api/event/EntityLifecycleEvents.kt
@@ -7,7 +7,9 @@ import net.minecraft.world.entity.Entity
 
 typealias EntitySpawn = (entity: Entity, level: ServerLevel) -> Unit
 
+@Deprecated("Deprecated in 4.1 and will be deleted in next major/minor version. Use Fabric ServerEntityEvents.ENTITY_LOAD event instead.")
 object EntityLifecycleEvents {
+    @Deprecated("Deprecated in 4.1 and will be deleted in next major/minor version. Use Fabric ServerEntityEvents.ENTITY_LOAD event instead.")
     @JvmField
     val ENTITY_SPAWN: Event<EntitySpawn> = EventFactory.createArrayBacked(EntitySpawn::class.java) { callbacks ->
         { a, b -> for (callback in callbacks) callback(a, b) }
