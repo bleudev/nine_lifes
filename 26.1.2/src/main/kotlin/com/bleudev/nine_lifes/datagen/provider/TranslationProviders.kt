7762b9aa1a4040489ef229d889b9efb3383fd444
bleudev <aitiiigg1@gmail.com>
2026-05-16 15:57:43 +0300

    done

--- 26.1.2/src/main/kotlin/com/bleudev/nine_lifes/datagen/provider/TranslationProviders.kt
+++ 26.1.2/src/main/kotlin/com/bleudev/nine_lifes/datagen/provider/TranslationProviders.kt
@@ -186,7 +186,7 @@ class NineLifesRussianTranslationProvider(output: FabricPackOutput, registriesFu
         builder.addConfigOption("category.general.root.option.low_lifes_red_sky",
             "Красное небо когда мало жизней", "Когда включено небо будет краснеть при низком количестве жизней")
         builder.addConfigOption("category.general.root.option.health_rendering",
-            "Health rendering", "Контроллирует рендеринг здоровья игрока\nХардкор - всегда рендерить хардкорные сердца\nИстинный хардкор - Только если у вас одна жизнь\nВанила - Ванильное поведение")
+            "Рендеринг здоровья", "Контроллирует рендеринг здоровья игрока\nХардкор - всегда рендерить хардкорные сердца\nИстинный хардкор - Только если у вас одна жизнь\nВанила - Ванильное поведение")
         builder.addConfigEnum(HeartPosition,
             "Снизу слева", "Снизу в центре", "Снизу справа",
             "Сверху слева", "Сверху в центре", "Сверху справа"
