833cd5f8ea5dff9c472e6864fee72511d1699a65
bleudev <aitiiigg1@gmail.com>
2026-05-17 12:33:14 +0300

    Prepare for 4.1

--- 26.1.2/build.gradle.kts
+++ 26.1.2/build.gradle.kts
@@ -115,7 +115,7 @@ modrinth {
     }
     changelog.set(project.property("changelog") as String)
     syncBodyFrom.set(project.property("readme") as String)
-    gameVersions.addAll("26.1")
+    gameVersions.addAll("26.1.2")
     loaders.add("fabric")
     dependencies {
         required.project("fabric-api")
