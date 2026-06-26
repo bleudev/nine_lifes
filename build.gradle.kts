3532792e35c45270896dd6e217903d286526f7a3
bleudev <aitiiigg1@gmail.com>
2026-06-26 17:43:37 +0300

    End of support for 1.21.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,5 @@ private fun String.snapshot(num: Int): Pair<String, String> = this to "$this-sna
 private fun String.pre(num: Int): Pair<String, String> = this to "$this-pre-$num"
 private fun String.rc(num: Int): Pair<String, String> = this to "$this-rc-$num"
 
-prConfigure("1.21.11", "26.1")
 prConfigure("26.1.2", "26.2")
 prConfigure("26.2", "26.3")
