507a8f1f655969c713fbf4385c1f43aec8b4a66e
bleudev <aitiiigg1@gmail.com>
2026-07-12 12:48:34 +0300

    fix: 26.1

--- 26.1.2/src/main/java/com/bleudev/nine_lifes/mixin/client/GuiMixin.java
+++ 26.1.2/src/main/java/com/bleudev/nine_lifes/mixin/client/GuiMixin.java
@@ -8,13 +8,13 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import static com.bleudev.nine_lifes.client.NineLifesClientStorageKt.getForceHardcore;
+import static com.bleudev.nine_lifes.client.NineLifesClientStorageKt.getForceHardcoreHearts;
 
 @Environment(EnvType.CLIENT)
 @Mixin(Gui.class)
 public class GuiMixin {
     @Redirect(method = "extractHearts", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/storage/LevelData;isHardcore()Z"))
     private boolean modifyIsHardcore(LevelData instance) {
-        return getForceHardcore() || instance.isHardcore();
+        return getForceHardcoreHearts() || instance.isHardcore();
     }
 }
