5e6c4d4887f3140dae2f31120ef39740fee6c42f
bleudev <aitiiigg1@gmail.com>
2026-07-16 19:55:08 +0300

    feat: 26.1.2 PostEffectRegistry.Builder

--- 26.1.2/src/main/kotlin/com/bleudev/nine_lifes/client/render/NineLifesPostRenderer.kt
+++ 26.1.2/src/main/kotlin/com/bleudev/nine_lifes/client/render/NineLifesPostRenderer.kt
@@ -1,7 +1,6 @@
 package com.bleudev.nine_lifes.client.render
 
-import com.bleudev.nine_lifes.api.render.client.DynamicUniformsRegistryImpl
-import com.bleudev.nine_lifes.api.render.client.PostEffectRegistryImpl
+import com.bleudev.nine_lifes.api.render.client.PostEffectRegistry
 import com.bleudev.nine_lifes.client.util.applyPostEffect
 import com.mojang.blaze3d.resource.CrossFrameResourcePool
 import net.minecraft.client.Minecraft
@@ -9,13 +8,10 @@ import net.minecraft.resources.Identifier
 
 class NineLifesPostRenderer(val resourcePool: CrossFrameResourcePool, val minecraftSupplier: () -> Minecraft) {
     init {
-        DynamicUniformsRegistryImpl.initBuffers()
+        PostEffectRegistry.initBuffers()
     }
 
-    fun render() {
-        DynamicUniformsRegistryImpl.updateBuffers()
-        PostEffectRegistryImpl.execute(::renderPostEffect)
-    }
+    fun render() = PostEffectRegistry.execute(::renderPostEffect)
 
     private fun renderPostEffect(postEffectId: Identifier) = minecraftSupplier().applyPostEffect(postEffectId, resourcePool)
 }
