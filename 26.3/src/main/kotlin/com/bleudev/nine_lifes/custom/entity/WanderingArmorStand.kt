0ccc242f706a7306f28dffcd6f5ca5f86758e49b
bleudev <95937737+bleudev@users.noreply.github.com>
2026-09-10 17:15:55 +0300

    feat: Wstand big update (#142)

--- 26.3/src/main/kotlin/com/bleudev/nine_lifes/custom/entity/WanderingArmorStand.kt
+++ 26.3/src/main/kotlin/com/bleudev/nine_lifes/custom/entity/WanderingArmorStand.kt
@@ -24,16 +24,24 @@ import net.minecraft.world.damagesource.DamageSource
 import net.minecraft.world.damagesource.DamageTypes
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.EntityType
+import net.minecraft.world.entity.EquipmentSlot
 import net.minecraft.world.entity.PathfinderMob
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier
 import net.minecraft.world.entity.ai.attributes.Attributes
 import net.minecraft.world.entity.ai.goal.TemptGoal
 import net.minecraft.world.entity.player.Player
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Items
+import net.minecraft.world.item.enchantment.EnchantmentEffectComponents
+import net.minecraft.world.item.enchantment.EnchantmentHelper
 import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.phys.Vec3
 
 class WanderingArmorStand(entityType: EntityType<out PathfinderMob>, level: Level) : PathfinderMob(entityType, level) {
     init { this.health = 1f }
+
+    var lastHit: Long = 0
 
     private fun sourceCanInstaKill(damageSource: DamageSource) = damageSource.`is`(DamageTypes.GENERIC_KILL)
     private fun sourceCanHit(damageSource: DamageSource) = sourceCanInstaKill(damageSource) ||
@@ -66,49 +74,135 @@ class WanderingArmorStand(entityType: EntityType<out PathfinderMob>, level: Leve
     override fun isPushable(): Boolean = false
     override fun push(entity: Entity) {}
     override fun doPush(entity: Entity) {}
+    override fun isAffectedByFluids(): Boolean = false
     override fun kill(serverLevel: ServerLevel) { if (!serverLevel.isClientSide) remove(RemovalReason.KILLED) }
-    private fun kill() = (level() as? ServerLevel)?.let { kill(it) }
-    override fun hurtServer(serverLevel: ServerLevel, damageSource: DamageSource, f: Float): Boolean {
-        if (!sourceCanHit(damageSource)) return false
-        val player = damageSource.directEntity as? ServerPlayer
+    private fun kill() {
+        for (slot in EquipmentSlot.VALUES) {
+            val itemStack = this.equipment.set(slot, ItemStack.EMPTY)
+            if (!itemStack.isEmpty && !EnchantmentHelper.has(itemStack, EnchantmentEffectComponents.PREVENT_EQUIPMENT_DROP)) {
+                Block.popResource(this.level(), this.blockPosition().above(), itemStack)
+            }
+        }
+        (level() as? ServerLevel)?.let { kill(it) }
+    }
+    override fun hurtServer(level: ServerLevel, source: DamageSource, damage: Float): Boolean {
+        if (!sourceCanHit(source)) return false
+        val player = source.directEntity as? ServerPlayer
         if (player != null && !playerCanHit(player)) return false
         this.kickTimes++
         this.ticksAfterKick = 0
-        val bl = this.kickTimes == WSTAND_KICK_TIMES || (player?.isCreative ?: false) || sourceCanInstaKill(damageSource)
+        val bl = this.kickTimes == WSTAND_KICK_TIMES || (player?.isCreative ?: false) || sourceCanInstaKill(source)
         val snd = if (bl) NineLifesSounds.ENTITY_WANDERING_ARMOR_STAND_DEATH else NineLifesSounds.ENTITY_WANDERING_ARMOR_STAND_HURT
         val pitch = if (bl) 0.8f else 0.95f
         val rad = if (bl) WSTAND_KILL_EVENT_RADIUS else WSTAND_KICK_EVENT_RADIUS
         level().playSound(null, x, y, z, snd, SoundSource.AMBIENT, 1f, pitch)
 
-        serverLevel.getPlayers { it.position().distanceToSqr(this.position()) <= rad*rad }.forEach {
+        level.getPlayers { it.position().distanceToSqr(this.position()) <= rad*rad }.forEach {
             it.sendPacket(ArmorStandHitEvent(this.position()))
             if (bl) it.sendPacket(ArmorStandKillEvent.INSTANCE)
         }
         if (bl) kill()
+        else lastHit = level.gameTime
         player?.let { triedKillReact(it) }
 
         return bl
     }
     override fun isInvulnerableTo(serverLevel: ServerLevel, damageSource: DamageSource): Boolean = !sourceCanInstaKill(damageSource)
-    override fun mobInteract(player: Player, hand: InteractionHand): InteractionResult {
-        if (player.getItemInHand(hand).`is`(Items.AMETHYST_SHARD)) {
-            player.consumeOneItemInHand(hand)
-            repeat(3) {
-                level().addParticle(ParticleTypes.HEART,
-                    getRandomX(1.0), randomY + 0.5, getRandomZ(1.0),
-                    random.nextGaussian() * 0.02, random.nextGaussian() * 0.02, random.nextGaussian() * 0.02
-                )
+    override fun interact(player: Player, hand: InteractionHand, location: Vec3): InteractionResult {
+        val itemStack = player.getItemInHand(hand)
+        if (!itemStack.`is`(Items.NAME_TAG)) {
+            if (player.isSpectator) {
+                return InteractionResult.SUCCESS
+            } else if (player.level().isClientSide) {
+                return InteractionResult.SUCCESS_SERVER
+            } else {
+                val itemInHandSlot = this.getEquipmentSlotForItem(itemStack)
+                if (itemStack.isEmpty) {
+                    val clickedSlot = this.getClickedSlot(location)
+                    val targetSlot = if (this.isDisabled(clickedSlot)) itemInHandSlot else clickedSlot
+                    if (this.hasItemInSlot(targetSlot) && this.swapItem(player, targetSlot, itemStack, hand)) {
+                        return InteractionResult.SUCCESS_SERVER
+                    }
+                } else {
+                    // WStand feeding
+                    if (itemStack.`is`(Items.AMETHYST_SHARD) && feed(player, hand)) {
+                        return InteractionResult.SUCCESS_SERVER
+                    }
+                    // Armor stand logic
+                    if (this.isDisabled(itemInHandSlot)) {
+                        return InteractionResult.FAIL
+                    }
+                    if (this.swapItem(player, itemInHandSlot, itemStack, hand)) {
+                        return InteractionResult.SUCCESS_SERVER
+                    }
+                }
+                return super.interact(player, hand, location)
             }
-            this.wanderTicks = WSTAND_WANDER_TICKS
-            return InteractionResult.SUCCESS
+        } else {
+            return super.interact(player, hand, location)
         }
-        return super.mobInteract(player, hand)
     }
+
     override fun tick() {
         super.tick()
         if (this.wanderTicks > 0) this.wanderTicks--
         if (this.ticksAfterKick < WSTAND_KICK_TICKS) this.ticksAfterKick++
         else if (this.ticksAfterKick == WSTAND_KICK_TICKS) this.kickTimes = 0
+    }
+
+    private fun feed(player: Player, hand: InteractionHand): Boolean {
+        player.consumeOneItemInHand(hand)
+        repeat(3) {
+            level().addParticle(ParticleTypes.HEART,
+                getRandomX(1.0), randomY + 0.5, getRandomZ(1.0),
+                random.nextGaussian() * 0.02, random.nextGaussian() * 0.02, random.nextGaussian() * 0.02
+            )
+        }
+        this.wanderTicks = WSTAND_WANDER_TICKS
+        return true
+    }
+
+    /** From `ArmorStand.getClickedSlot` */
+    private fun getClickedSlot(location: Vec3): EquipmentSlot {
+        var slotClicked: EquipmentSlot = EquipmentSlot.MAINHAND
+        val clickYPosition = location.y / (this.scale * this.ageScale).toDouble()
+        if (clickYPosition in 0.1..<0.55 && this.hasItemInSlot(EquipmentSlot.FEET)) {
+            slotClicked = EquipmentSlot.FEET
+        } else if (clickYPosition in 0.9..<1.6 && this.hasItemInSlot(EquipmentSlot.CHEST)) {
+            slotClicked = EquipmentSlot.CHEST
+        } else if (clickYPosition in 0.4..<1.2 && this.hasItemInSlot(EquipmentSlot.LEGS)) {
+            slotClicked = EquipmentSlot.LEGS
+        } else if (clickYPosition >= 1.6 && this.hasItemInSlot(EquipmentSlot.HEAD)) {
+            slotClicked = EquipmentSlot.HEAD
+        } else if (!this.hasItemInSlot(EquipmentSlot.MAINHAND) && this.hasItemInSlot(EquipmentSlot.OFFHAND)) {
+            slotClicked = EquipmentSlot.OFFHAND
+        }
+        return slotClicked
+    }
+
+    /** From `ArmorStand.isDisabled`, but modified */
+    private fun isDisabled(slot: EquipmentSlot): Boolean {
+        return slot.type == EquipmentSlot.Type.HAND
+    }
+
+    /** From `ArmorStand.swapItem`, but modified */
+    private fun swapItem(player: Player, slot: EquipmentSlot, playerItemStack: ItemStack, hand: InteractionHand): Boolean {
+        val itemStack = this.getItemBySlot(slot)
+        if (player.hasInfiniteMaterials() && itemStack.isEmpty && !playerItemStack.isEmpty) {
+            this.setItemSlot(slot, playerItemStack.copyWithCount(1))
+            return true
+        } else if (playerItemStack.count > 1) {
+            if (!itemStack.isEmpty) {
+                return false
+            } else {
+                this.setItemSlot(slot, playerItemStack.split(1))
+                return true
+            }
+        } else {
+            this.setItemSlot(slot, playerItemStack)
+            player.setItemInHand(hand, itemStack)
+            return true
+        }
     }
 
     val canWander: Boolean
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,11 +2,31 @@
 
 ## Targets
 
-| Minecraft Version | Fabric |
-|-------------------|--------|
-| 26.3-snapshot-9   | ✅️     |
-| 26.2              | ✅️     |
-| 26.1.2            | ✅️     |
+| Minecraft Version | Fabric                                           |
+|-------------------|--------------------------------------------------|
+| 26.3-snapshot-9   | ✅️                                               |
+| 26.2              | ✅️                                               |
+| 26.1.2            | ⚠️ (Will no longer supported after 26.3 release) |
 
 ## Changes
 
+### Dependencies
+
+- [DEV] Gradle 9.6.1 (bleudev [#142](https://github.com/bleudev/nine_lifes/pull/142))
+
+### New features
+
+#### Wandering armor stand
+
+- You can now hang armor, elytras, heads etc. like with normal armor stands!!! (bleudev [#142](https://github.com/bleudev/nine_lifes/pull/142))
+- Fluids won't affect wandering armor stand anymore (I hope) (bleudev [#142](https://github.com/bleudev/nine_lifes/pull/142))
+
+#### Public API [DEV]
+
+- `Inventory.anyWithContainers(predicate)`: returns if there is any item in inventory (including items in containers, bundles, etc.) matching given `predicate` (bleudev [#142](https://github.com/bleudev/nine_lifes/pull/142))
+- `ItemStack.isCharged(...)`: returns if this item stack is charged. See function definition for arguments info. (bleudev [#142](https://github.com/bleudev/nine_lifes/pull/142))
+
+### Fixes
+
+- Containers, bundles etc. weren't considered in players with charged items finder (bleudev [#142](https://github.com/bleudev/nine_lifes/pull/142))
+- Deleted arms from wandering armor stand by default (without code modification) (bleudev [#142](https://github.com/bleudev/nine_lifes/pull/142))
